8557e4dace338d415ae4bfc5a3b6555a311f3b2a
JulianKer <jschmuker742@alumno.unlam.edu.ar>
2025-07-06 05:48:02 -0300

    agrego data.sql con imports de productos

---
+++ back/data/productos.sql
@@ -0,0 +1,11 @@
+-- hacer este instert en la bdd en mysql para cargar algunos productos
+
+INSERT INTO `producto` (`id`, `nombre`, `descripcion`, `clasificacion`, `precio`) VALUES
+(14, 'auto_alpine.avif', 'Auto Alpine - Edición 2024', 'Auto a escala', 74),
+(15, 'auto_astonmartin.avif', 'Auto Aston Martin - Edición 2024', 'Auto a escala', 59.99),
+(16, 'auto_haas.webp', 'Auto Haas - Edición 2024', 'Auto a escala', 89.5),
+(17, 'auto_maclaren.avif', 'Auto MClaren - Edición 2024', 'Auto a escala', 120),
+(18, 'auto_redbull.webp', 'Auto RedBull - Edición 2024', 'Auto a escala', 15),
+(19, 'auto_mercedes.avif', 'Auto Mercedes - Edición 2024', 'Auto a escala', 92.3),
+(20, 'auto_stake.avif', 'Auto Stake - Edición 2024', 'Auto a escala', 48.75),
+(21, 'auto_ferrari.avif', 'Auto Ferrari - Edición 2024', 'Auto a escala', 70);
